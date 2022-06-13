a5f6cd5a12bd8eda6a0ac7f27f1a0def3da01b9c
Singgih Pratama <78544446+mastama@users.noreply.github.com>
2022-06-13 20:33:27 +0700

    create service and impl

---
+++ src/main/java/com/example/loginregist/service/UserDetailsService.java
@@ -0,0 +1,8 @@
+package com.example.loginregist.service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public interface UserDetailsService {
+    UserDetails loadUserByUsername(String username) throws UsernameNotFoundException;
+}
