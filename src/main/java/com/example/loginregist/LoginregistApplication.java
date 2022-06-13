4866e62deba74964bad50744d932c043de7c6ab9
Singgih Pratama <78544446+mastama@users.noreply.github.com>
2022-06-13 13:05:55 +0700

    startpack is ready to use

---
+++ src/main/java/com/example/loginregist/LoginregistApplication.java
@@ -0,0 +1,13 @@
+package com.example.loginregist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginregistApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoginregistApplication.class, args);
+    }
+
+}
